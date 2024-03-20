2e5747ce69ee7e20d7fec6d89e0e83f54c80884b
Monalisha Panda <65297094+monalisha98452@users.noreply.github.com>
2024-03-20 16:24:05 +0530

    Number.java

--- Number.java
+++ Number.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
-
+// Print all even numbers till n
+import java.util.*;
 public class Number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
