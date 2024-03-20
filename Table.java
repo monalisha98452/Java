950ffceae1101d6dbc8dafcde4147000831fd3db
Monalisha Panda <65297094+monalisha98452@users.noreply.github.com>
2024-03-20 16:21:29 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,5 +1,5 @@
+// Print the table of a number input by the user.
 import java.util.*;
-
 public class Table {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
